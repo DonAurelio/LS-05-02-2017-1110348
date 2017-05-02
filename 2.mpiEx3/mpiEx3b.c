ab4a5cf37183bc90260425e67f23fb8e36fcf239
invitado <invitado@s1pc7.eisc.univalle.edu.co>
2017-05-02 16:58:54 -0500

    Third exersice

---
+++ 2.mpiEx3/mpiEx3b.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc,char *argv[]){
+	int size, rank, dest, source, count, tag=1;
+	int inmsg, outmsg;
+	MPI_Status Stat;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == 0) {
+	  dest = 1;
+	  source = size - 1;
+	  outmsg = rank;
+	  MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+	  MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+      printf("Soy el proceso %d y recibo %d\n", rank, inmsg);
+	}else{
+	  dest = (rank == size - 1) ? 0 : rank + 1;
+	  source = rank - 1;
+	  MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+	  printf("Soy el proceso %d y recibo %d\n", rank, inmsg);
+	  outmsg = rank;
+	  MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+	 }
+
+
+	//MPI_Get_count(&Stat, MPI_INT, &count);
+	//printf("Task %d: Received %d char(s) from task %d with tag %d \n",
+	//	   rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
+
+	MPI_Finalize();
+}
+
+
+
--- Makefile
+++
@@ -1,8 +0,0 @@
-run: compile
-	mpirun -np 2 --hostfile machinefile ./mpiEx2b
-	mpirun -np 4 --hostfile machinefile ./mpiEx2b
-	mpirun -np 8 --hostfile machinefile ./mpiEx2b
-compile:
-	mpicc mpiEx2b.c -o ./mpiEx2b
-clean:
-	rm -rf ./mpiEx2b

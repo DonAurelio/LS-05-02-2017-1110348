f16659f0ece1c5410333446da2f7d1c65b7a471d
invitado <invitado@s1pc7.eisc.univalle.edu.co>
2017-05-09 15:58:35 -0500

    MPI Reduction ny hand

--- 3.mpiEx4/mpiEx4a.c
+++ 3.mpiEx4/mpiEx4a.c
@@ -1,9 +1,13 @@
+/*
+	MPI Reduccion manual
+*/
 #include <stdio.h>
 #include "mpi.h"
 
 int main(int argc,char *argv[]){
-	int i, sum, sumTotal, upToVal;
+	int i, sum=0, sumTotal=0, upToVal, insum=0;
 	int start, end, size, rank;
+	MPI_Status Stat;
 
 	upToVal = 10000;
 
@@ -22,10 +26,23 @@ int main(int argc,char *argv[]){
 	for(i=start; i<= end; i++){
 		sum = sum +i;
 	}
-	// Utilice la funcion 'MPI_Reduce' para guardar en la variable 
-	// 'sumTotal' la suma parcial de todos las tareas 
-	MPI_Reduce(&sum,&sumTotal,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-	if (rank == 0) printf ("\nTotal: %d\n",sumTotal);
+
+	if( rank == 0 ){
+
+		/* Recibiendo sumas parciales de procesos de rank > 0*/
+		for(int i=1;i<size;i++){
+			MPI_Recv(&insum, 1, MPI_INT, i, 1, MPI_COMM_WORLD,&Stat);
+			sumTotal += insum;
+		}
+		/* sumando la suma parcial del proceso con rank 0 */
+		sumTotal = sum;
+		/* resultado total */
+		printf ("\nTotal: %d\n",sumTotal);
+
+	}else{
+		/* enviando suma parcial al proceso con rank 0 */
+		MPI_Send(&sum,1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	}
 
 	MPI_Finalize();
 	
